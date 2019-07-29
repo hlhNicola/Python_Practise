498c63842230839821de3e11cd7019ad029d7ff7
Lihong Han <littlefly14@gmail.com>
2019-07-28 20:19:08 -0400

    advance cal

--- Playground/calculator.py
+++ Playground/calculator.py
@@ -1,5 +1,24 @@
+# simple one
+
 num1 = input("Enter a number: ")
 num2 = input("Enter another number: ")
 result = num1 + num2
 
 print(result)
+
+# advanced one
+
+num1 = input("Enter first number")
+op = input("Enter operator: ")
+num2 = input("Enter another number: ")
+
+if op == "+":
+    print(num1 + num2)
+elif op == "-":
+    print(num1 - num2)
+elif op == "*":
+    print(num1 * num2)
+elif op == "/":
+    print(num1 / num2)
+else:
+    print("invald calculation")
---
+++ Playground/list_tuple.py
@@ -0,0 +1,7 @@
+# tuple cannot be changed
+tuples = (4, 5)
+lists = [4, 5]
+
+lists[0] = 1
+tuples[0] = 1
+print(lists, tuples)
